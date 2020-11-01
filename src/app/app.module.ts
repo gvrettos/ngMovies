311dbed6d93f08c870ddba70c055b81bb5acb65b
George Vrettos <lukedevgr@gmail.com>
2020-11-01 16:50:34 +0200

    Create movie-details component and connect movie preview and details with master/detail relationship

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule
