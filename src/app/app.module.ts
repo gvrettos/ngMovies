abc3949b369572a87f48adbe332cfa852c1bba0f
George Vrettos <lukedevgr@gmail.com>
2020-11-08 00:56:38 +0200

    Create navbar component to wrap the router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
-    MovieDetailComponent
+    MovieDetailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
